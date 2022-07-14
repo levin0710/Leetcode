78db0e0d6dc148f519b93d6d01ccb4dc8c1f94ad
Levin Sanchez <94712128+levin0710@users.noreply.github.com>
2022-07-14 10:26:38 -0400

    Time: 67 ms (18.73%), Space: 13.9 MB (24.29%) - LeetHub

--- 67-add-binary/67-add-binary.py
+++ 67-add-binary/67-add-binary.py
@@ -1,16 +1,19 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        answer = ""
-        carry, a, b = 0, a[::-1], b[::-1]
         
+        carry, a, b = 0, a[::-1], b[::-1]
+        answer = ""
         for i in range(max(len(a), len(b))):
             num1 = int(a[i]) if i < len(a) else 0
             num2 = int(b[i]) if i < len(b) else 0
             
-            ones = num1 + num2 + carry
-            answer = str(ones % 2) + answer  
-            carry = ones // 2
-        if carry != 0:
-            answer = "1" + answer
-        return answer
+            numOnes = carry + num1 + num2
             
+            answer = str(numOnes % 2) + answer
+            
+            carry = numOnes // 2
+            
+        if carry != 0:
+             answer = "1" + answer
+                
+        return answer
