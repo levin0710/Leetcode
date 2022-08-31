738d65a0a1a5b5bc78f844b009b5399a8cd6ca4f
Levin Sanchez <94712128+levin0710@users.noreply.github.com>
2022-08-31 14:41:50 +0200

    Time: 309 ms (68.60%), Space: 21.4 MB (53.93%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.py
+++ 238-product-of-array-except-self/238-product-of-array-except-self.py
@@ -3,12 +3,16 @@ class Solution:
         
         answer = [1 for num in nums]
         
-        for i in range(1, len(nums)):
-            answer[i] = nums[i - 1] * answer[i-1]
+        multiplier = 1
         
+        for i in range(len(nums)):
+            answer[i] *= multiplier
+            multiplier *= nums[i]
+            
         multiplier = 1
         
         for i in reversed(range(len(nums))):
             answer[i] *= multiplier
             multiplier *= nums[i]
+            
         return answer
